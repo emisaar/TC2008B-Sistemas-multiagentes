7ae7c247cf2a1e62c0d4679ce004893a66950af9
AlejandroDiaz10 <75130652+AlejandroDiaz10@users.noreply.github.com>
2022-11-21 10:06:17 -0600

    Add files via upload

---
+++ Evidencia1-Parte1/Agents.py
@@ -0,0 +1,232 @@
+# Alejandro Díaz Villagómez - A01276769
+
+# Fecha - 21/Noviembre/2022
+# Evidencia 1
+
+import math
+
+from mesa import Agent
+
+# AGENTES
+#Estantes - unique_id > 100
+class Shelf(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+#Cajas - unique_id [10-30]
+class Box(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+#Pilas de cajas - unique_id [50-53]
+class StackBox(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.isFull = False
+        self.box_counting = 0
+    
+
+#Cajas - unique_id [1-5]
+class Robot(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.personal_steps = 0
+        self.hasBox = False
+        self.dist2stack = 100
+        self.destination = (100, 100)
+        self.boxes_delivered = 0
+
+    # ¿Qué pasará en cada unidad de tiempo?
+    def step(self):
+        #Si no tenemos una caja y la celda actual no es una caja, nos movemos de manera random
+        if self.hasBox == False and self.order_box() == False:
+            self.moveRandom()
+        #Si recogemos una caja, no nos movemos 1 step
+        elif self.order_box():
+            pass
+        #Si tenemos una caja, no podemos cargar más. Nos movemos a una pila
+        elif self.hasBox:
+            self.move2Stack()
+
+    # Buscamos los movimientos posibles de manera aleatoria
+    def moveRandom(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            # Posición actual
+            self.pos,
+            # Vecinos en diagonal
+            moore=False,
+            # Puede que un agente no se mueva
+            #include_center=True
+            include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        #La nueva posición es un estante, cambiamos rumbo
+        if new_position in self.model.shelf_coord:
+            possible_steps.remove(new_position)
+            new_position = self.random.choice(possible_steps)
+
+        # Checamos que la nueva posición no coincida con ningún otro robot
+        cellmates = self.model.grid.get_cell_list_contents([new_position])
+        if len(cellmates) > 0:
+            for c in cellmates:
+                if c.unique_id < 6:
+                    self.model.grid.move_agent(self, self.pos)
+                    return
+        # Si nos podemos mover, aumentamos el conteo de movimientos del agente
+        self.personal_steps += 1
+        self.model.grid.move_agent(self, new_position)
+    
+    def move2Stack(self):
+        #Si el robot está cargando una caja, debe encontrar la pila más cercana disponible,
+        #para ello, determinamos la pos actual del robot y el estado de cada pila
+        #Es necesario calcular en cada step estos elementos por si se llenan las pilas
+        x, y = self.pos
+        available_stacks = []
+        for agent in self.model.schedule.agents:
+            if agent.unique_id >= 50 and agent.unique_id <= 53 and agent.isFull == False:
+                available_stacks.append(agent.pos)
+        
+        #Calculamos las distancias del robot a las pilas
+        for i in range(len(available_stacks)):
+            dist = math.dist(self.pos, available_stacks[i])
+            if dist < self.dist2stack:
+                self.dist2stack = dist
+                self.destination = available_stacks[i]
+        
+        #Tenemos 4 opciones de destino (4 pilas)
+        #Cuadrante 1
+        if self.destination == (11, 11):
+            #Nos movemos según nuestra posición actual
+            x_act, y_act = self.pos
+            #Primero nos alineamos en el eje "x"
+            if x_act < 11:
+                new_position = (x + 1, y)
+            elif x_act > 11:
+                new_position = (x - 1, y)
+            
+            #Estamos alineados en "x", ahora nos movemos a y = 11
+            elif y_act < 11:
+                new_position = (x, y + 1)
+            elif y_act > 11:
+                new_position = (x, y - 1)
+
+            #Ya estamos en la posición final
+            else:
+                self.hasBox = False
+                self.boxes_delivered += 1
+                self.model.unordered_boxes -= 1
+                return
+            
+            # Checamos que la nueva posición no coincida con ningún otro robot
+            cellmates = self.model.grid.get_cell_list_contents([new_position])
+            if len(cellmates) > 0:
+                for c in cellmates:
+                    if c.unique_id < 6:
+                        self.model.grid.move_agent(self, self.pos)
+                        return
+            
+            # Si nos podemos mover, aumentamos el conteo de movimientos del agente
+            self.personal_steps += 1
+            self.model.grid.move_agent(self, new_position)
+
+            return
+
+        #Cuadrante 2
+        elif self.destination == (1, 11):
+            x_act, y_act = self.pos
+            if x_act < 1:
+                new_position = (x + 1, y)
+            elif x_act > 1:
+                new_position = (x - 1, y)
+            elif y_act < 11:
+                new_position = (x, y + 1)
+            elif y_act > 11:
+                new_position = (x, y - 1)
+            else:
+                self.hasBox = False
+                self.boxes_delivered += 1
+                self.model.unordered_boxes -= 1
+                return
+            
+            cellmates = self.model.grid.get_cell_list_contents([new_position])
+            if len(cellmates) > 0:
+                for c in cellmates:
+                    if c.unique_id < 6:
+                        self.model.grid.move_agent(self, self.pos)
+                        return
+            
+            self.personal_steps += 1
+            self.model.grid.move_agent(self, new_position)
+
+            return
+
+        #Cuadrante 3
+        elif self.destination == (1, 1):
+            x_act, y_act = self.pos
+            if x_act < 1:
+                new_position = (x + 1, y)
+            elif x_act > 1:
+                new_position = (x - 1, y)
+            elif y_act < 1:
+                new_position = (x, y + 1)
+            elif y_act > 1:
+                new_position = (x, y - 1)
+            else:
+                self.hasBox = False
+                self.boxes_delivered += 1
+                self.model.unordered_boxes -= 1
+                return
+            
+            cellmates = self.model.grid.get_cell_list_contents([new_position])
+            if len(cellmates) > 0:
+                for c in cellmates:
+                    if c.unique_id < 6:
+                        self.model.grid.move_agent(self, self.pos)
+                        return
+            
+            self.personal_steps += 1
+            self.model.grid.move_agent(self, new_position)
+
+            return
+
+        #Cuadrante 4
+        elif self.destination == (11, 1):
+            x_act, y_act = self.pos
+            if x_act < 11:
+                new_position = (x + 1, y)
+            elif x_act > 11:
+                new_position = (x - 1, y)
+            elif y_act < 1:
+                new_position = (x, y + 1)
+            elif y_act > 1:
+                new_position = (x, y - 1)
+            else:
+                self.hasBox = False
+                self.boxes_delivered += 1
+                self.model.unordered_boxes -= 1
+                return
+            
+            cellmates = self.model.grid.get_cell_list_contents([new_position])
+            if len(cellmates) > 0:
+                for c in cellmates:
+                    if c.unique_id < 6:
+                        self.model.grid.move_agent(self, self.pos)
+                        return
+            
+            self.personal_steps += 1
+            self.model.grid.move_agent(self, new_position)
+
+            return
+
+
+    # Función para recoger las cajas
+    def order_box(self):
+        # Recoge la caja en la celda en la que estoy (si actualmente no estoy cargando una)
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 0:
+            for c in cellmates:
+                if c.unique_id > 5 and c.unique_id < 40 and self.hasBox == False:
+                    self.model.grid.remove_agent(c)
+                    self.hasBox = True
+                    return True
+        return False
---
+++ Evidencia1-Parte1/Model.py
@@ -0,0 +1,181 @@
+# Alejandro Díaz Villagómez - A01276769
+# Fecha - 18/Noviembre/2022
+# Simulación de una intersección
+
+from mesa import Model
+from random import randint
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+from mesa import batch_run
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from Agents import *
+
+# Calculamos los movimientos totales de todos los agentes
+def show_agent_moves(model):
+    tot_steps = []
+    for a in model.schedule.agents:
+        if a.unique_id < 6:
+            tot_steps.append(a.personal_steps)
+    return sum(tot_steps)
+
+def show_agent_ordered_boxes(model):
+    tot_boxes = []
+    for a in model.schedule.agents:
+        if a.unique_id < 6:
+            tot_boxes.append(a.boxes_delivered)
+    return sum(tot_boxes)
+
+def show_unordered_boxes(model):
+    return model.unordered_boxes
+
+
+# MODELO
+class OrderingRobotsModel(Model):
+    def __init__(self, N, width, height):
+        self.steps = 0
+        self.num_cells = width * height
+        self.num_agents = N
+        self.shelf_coord = []
+        self.stack_box_coord = [(1, 1), (11, 1), (1, 11), (11, 11)]
+        self.unordered_boxes = 0
+
+        # Espacio físico para los agentes
+        # No permitimos que se salgan del mapa
+        self.grid = MultiGrid(width, height, False)
+        self.schedule = RandomActivation(self)
+        self.running = True
+
+        # Creamos una lista con coordenadas únicas para las "x" - apilamiento de cajas
+        for i in range(len(self.stack_box_coord)):
+            sb = StackBox(i + 50, self)
+            self.schedule.add(sb)
+            self.grid.place_agent(sb, self.stack_box_coord[i])
+
+        # Creamos una lista con coordenadas únicas para los estantes
+        for i in range (2, 11):
+            # shelf_coord.append((3, i))
+            # shelf_coord.append((9, i))
+            self.shelf_coord.append((i, 3))
+            self.shelf_coord.append((i, 9))
+        
+        # Creamos una lista con coordenadas únicas para las cajas
+        box_coord = []
+        k = randint(8, 20)
+        self.unordered_boxes = self.total_boxes = k
+        print("Boxes: " + str(k))
+        while len(box_coord) < k:
+            box_pos = (randint(0, height - 1), randint(0, width - 1))
+            if box_pos not in self.shelf_coord and box_pos not in self.stack_box_coord and box_pos not in box_coord:
+                box_coord.append(box_pos)
+        
+        # Creamos una lista con coordenadas únicas para los robots
+        robot_coord = []
+        while len(robot_coord) < self.num_agents:
+            robot_pos = (randint(0, height - 1), randint(0, width - 1))
+            if robot_pos not in self.shelf_coord and robot_pos not in self.stack_box_coord and robot_pos not in box_coord and robot_pos not in robot_coord:
+                robot_coord.append(robot_pos)
+        
+
+        # Agregamos los agentes al mapa
+        x = j = z = 0
+        for i in range(1, self.num_agents + len(self.shelf_coord) + k + 1):
+            if i <= self.num_agents:
+                r = Robot(i, self)
+                self.schedule.add(r)
+                self.grid.place_agent(r, robot_coord[x])
+                x += 1
+            elif i > self.num_agents and i <= self.num_agents + len(self.shelf_coord):
+                s = Shelf(100 + j, self)
+                self.schedule.add(s)
+                self.grid.place_agent(s, self.shelf_coord[j])
+                j += 1
+            else:
+                b = Box(10 + z, self)
+                self.schedule.add(b)
+                self.grid.place_agent(b, box_coord[z])
+                z += 1
+        
+
+
+        # Metrics we will measure about our model
+        self.datacollector = DataCollector(
+            model_reporters={
+                "Movimientos_Agentes": show_agent_moves,
+                "Cajas_Ordenadas": show_agent_ordered_boxes,
+                "Cajas_Desordenadas": show_unordered_boxes,
+            },
+        )
+
+    # ¿Qué pasará en cada unidad de tiempo?
+    def step(self):
+        # Corre 1 tick en el reloj de nuestra simulación
+        self.steps += 1
+        self.datacollector.collect(self)
+        self.schedule.step()
+        
+        #Debemos checar el porcentaje de cajas ordenadas (25%)
+        self.percentage = show_agent_ordered_boxes(self) * 100 / self.total_boxes
+        print("Cajas totales: " + str (self.total_boxes))
+        print("Ordenados: "  + str (show_agent_ordered_boxes(self)))
+        print("Desordenados: "  + str (show_unordered_boxes(self)))
+        print("Porcentaje ordenad: " + str(self.percentage))
+        if self.percentage >= 25:
+            self.running = False
+    
+    def run_model(self):
+        while self.running:
+            self.step()
+
+
+# Parámetros para correr el modelo varias veces
+model_params = {
+    "N": 5,
+    "width": 13,
+    "height": 13
+}
+
+# Usaremos batch_run
+results = batch_run(
+    OrderingRobotsModel,
+    parameters=model_params,
+    iterations=100,
+    number_processes=1,
+    data_collection_period=1,
+    display_progress=False,
+)
+
+# Convertimos los resultados en un DataFrame
+results_df = pd.DataFrame(results)
+print(results_df.keys())
+
+# Agrupamos la información y obtenemos solo los últimos valores
+grouped_iterations = pd.DataFrame(
+    columns=['iteration', 'Step', 'Movimientos_Agentes', 'Cajas_Ordenadas', 'Cajas_Desordenadas'])
+
+for it, group in results_df.groupby(["iteration"]):
+    grouped_iterations = grouped_iterations.append(
+        {'iteration': group.iloc[-1].iteration,
+         'Step': group.iloc[-1].Step,
+         'Movimientos_Agentes': group.iloc[-1].Movimientos_Agentes,
+         'Cajas_Ordenadas': group.iloc[-1].Cajas_Ordenadas,
+         'Cajas_Desordenadas': group.iloc[-1].Cajas_Desordenadas},
+        ignore_index=True)
+#print(grouped_iterations.to_string(index=False, max_rows=25))
+print(grouped_iterations.to_string(index=False))
+
+# Hacemos una gráfica que representa el tiempo necesario para ordenar el 25% de
+# de las cajas en las 100 iteraciones
+# El grid es de 10x10 (100 celdas) y el tiempo máximo es de 50 pasos
+sns.set_theme()
+sns.scatterplot(
+    data=grouped_iterations,
+    x="iteration", y="Step",
+)
+plt.title('Tiempo para apilar las cajas (25%)')
+plt.xlabel('Iteraciones')
+plt.ylabel('Steps')
+plt.show()
